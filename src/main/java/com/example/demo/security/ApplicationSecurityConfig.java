5629e2679f68df0ac1045e6edceb6231a3f16545
jagadeeswar reddy <jagadeeshwar8919@gmail.com>
2021-07-19 12:20:58 +0530

    Added password encoder implementation

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -1,14 +1,27 @@
 package com.example.demo.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter{
-	
+	private final PasswordEncoder passwordEncoder;
+
+	@Autowired
+	public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+		this.passwordEncoder = passwordEncoder;
+	}
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
@@ -20,4 +33,16 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter{
 		    .httpBasic();
 	}
 
+	@Override
+	@Bean
+	protected UserDetailsService userDetailsService() {
+		UserDetails jaiuser=User.builder()
+				                     .username("jagadeesh")
+				                      .password(passwordEncoder.encode("Jaga@9951"))
+				                       .roles("User").build();
+
+		return new InMemoryUserDetailsManager(
+				jaiuser
+		);
+	}
 }
